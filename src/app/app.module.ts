6d9cb93b7a42d0f3a01769d6d4329816775abe18
Gustavo de Paula <gdepaula@gmail.com>
2021-02-16 18:12:32 -0300

    Declare HeroesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 @NgModule({
