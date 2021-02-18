d873192b2a77ce599cbb5f0fa8308b894397b808
Gustavo de Paula <gdepaula@gmail.com>
2021-02-18 19:42:16 -0300

    Fix Bug - type 'Observable<X | undefined' is not assignable

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -20,13 +20,13 @@ export class HeroDetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // this.getHero();
+    this.getHero();
   }
 
-  // getHero(): void {
-  //   const id = Number(this.route.snapshot.paramMap.get('id'));
-  //   this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
-  // }
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
 
   goBack(): void {
     this.location.back();
